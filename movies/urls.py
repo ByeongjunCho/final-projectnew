e0eed16a4ac3637876761f1d75507f7da0ce82d4
[user_name] <[user_email]>
2019-11-24 14:01:29 +0900

    first commit

---
+++ movies/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'movies'
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('<int:movie_id>/',views.detail,name='detail'),
+    path('<int:movie_id>/create/',views.review_create,name='review_create'),
+    path('<int:movie_id>/create/<int:review_id>/delete/',views.review_delete,name='review_delete'),
+    path('<int:movie_id>/like/',views.like,name='like'),
+
+]
